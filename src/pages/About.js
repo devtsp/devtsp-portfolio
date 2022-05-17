f5549c6c195233ef386b028c07b02eb5a28f6e04
Tomas <87679143+devtsp@users.noreply.github.com>
2022-05-17 16:48:47 -0300

    about update

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,26 +9,22 @@ const About = () => {
 				technologies and strengthen my programming fundamentals.
 			</p>
 			<p>
-				I understand the importance of well written <strong>tests</strong>,
-				proper folder tree <strong>organization</strong> and{' '}
-				<strong>naming conventions,</strong> code
-				<strong> legibility/mantainability</strong>, and I will drop those juicy{' '}
+				I strive to write <strong>lean</strong> and
+				<strong> self-explanatory tests</strong>, implement proper
+				<strong> folder tree organization</strong>, follow popular
+				<strong> naming conventions,</strong> keep code
+				<strong> legibility</strong> and drop those juicy{' '}
 				<a href="https://www.alpharithms.com/solid-guidelines-for-better-software-development-055500/">
 					SOLID
 				</a>{' '}
-				principles wherever I can.
+				principles whenever I can.
 			</p>
-			<p>
-				My philosophy is to try to be the one who knows the least in the room;
-				so I can grow and learn from the ones with greater experience around me.
-			</p>
-			<div id="separator"></div>
 			<p>
 				You can contact me at{' '}
 				<a href="mailto:paeztms@gmail.com" target="_blank" rel="noreferrer">
 					paeztms@gmail.com
 				</a>{' '}
-				if some of these concepts resonate with your organization's principles
+				if some of these concepts resonates with your project's requirements
 			</p>
 		</div>
 	);
