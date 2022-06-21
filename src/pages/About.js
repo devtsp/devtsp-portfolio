87711bd6ff4a233fd7c91efa9c565eed9d734fff
Tomas <87679143+devtsp@users.noreply.github.com>
2022-06-21 19:56:19 -0300

    change about description

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,27 +1,21 @@
+import { Link } from 'react-router-dom';
+
 const About = () => {
 	return (
 		<div id="About">
 			<h1> &lt; Fullstack Javascript developer /&gt; </h1>
+			<div className="separator"></div>
 			<p>
-				Constantly working on different kind of projects to keep learning new
-				technologies and strengthen my programming fundamentals.
+				Constantly working on different kind of projects to expand my skills and
+				strengthen the fundamentals.
+			</p>
+			<p>
+				I prefer to let my work speak by itself. Take a look at some of my{' '}
+				<Link to="/projects">PROJECTS</Link>
 			</p>
 			<div className="separator"></div>
 			<p>
-				I strive to write <strong>comprehensive</strong> and
-				<strong> self-explanatory tests</strong>, implement proper
-				<strong> folder tree organization</strong>, follow understandable
-				<strong> naming conventions,</strong> keep code
-				<strong> legibility</strong> and drop those juicy{' '}
-				<a href="https://www.alpharithms.com/solid-guidelines-for-better-software-development-055500/">
-					SOLID
-				</a>{' '}
-				principles whenever I can.
-			</p>
-			<div className="separator"></div>
-			<p>
-				If some of these concepts resonates with your project's requirements,
-				you can contact me at{' '}
+				You can contact me at{' '}
 				<a href="mailto:paeztms@gmail.com" target="_blank" rel="noreferrer">
 					paeztms@gmail.com
 				</a>
