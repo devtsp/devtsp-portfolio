f70b30e31e3cfd2df6c9e1617907e7adf64bdd70
Tomas <87679143+devtsp@users.noreply.github.com>
2022-06-21 19:34:03 -0300

    add stocktaking-server project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -16,14 +16,15 @@ const Projects = () => {
 
 const projects = [
 	{
-		title: 'social media',
-		deployed: true,
-		sourceCode: 'https://github.com/devtsp/social-media-app-client',
-		site: 'https://delightful-cascaron-53270c.netlify.app/',
+		title: 'stocktaking',
+		deployed: false,
+		sourceCode: 'https://github.com/devtsp/stocktaking-server',
+		site: 'https://devtsp.github.io/pokedex-vanilla-client/',
 		previews: [
-			'/img/project%20previews/social%20media/1.jpg',
-			'/img/project%20previews/social%20media/2.jpg',
-			'/img/project%20previews/social%20media/3.jpg',
+			'/img/project%20previews/stocktaking/1.jpg',
+			'/img/project%20previews/stocktaking/2.jpg',
+			'/img/project%20previews/stocktaking/3.jpg',
+			'/img/project%20previews/stocktaking/4.jpg',
 		],
 	},
 	{
@@ -48,6 +49,27 @@ const projects = [
 		],
 	},
 	{
+		title: 'social media',
+		deployed: true,
+		sourceCode: 'https://github.com/devtsp/social-media-app-client',
+		site: 'https://delightful-cascaron-53270c.netlify.app/',
+		previews: [
+			'/img/project%20previews/social%20media/1.jpg',
+			'/img/project%20previews/social%20media/2.jpg',
+			'/img/project%20previews/social%20media/3.jpg',
+		],
+	},
+	{
+		title: 'pokedex',
+		deployed: true,
+		sourceCode: 'https://github.com/devtsp/pokedex-vanilla-client',
+		site: 'https://devtsp.github.io/pokedex-vanilla-client/',
+		previews: [
+			'/img/project%20previews/pokedex/1.jpg',
+			'/img/project%20previews/pokedex/2.jpg',
+		],
+	},
+	{
 		title: 'memotest',
 		deployed: true,
 		sourceCode: 'https://github.com/devtsp/memotest-client',
@@ -68,16 +90,6 @@ const projects = [
 			'/img/project%20previews/clubs/2.jpg',
 			'/img/project%20previews/clubs/3.jpg',
 			'/img/project%20previews/clubs/4.jpg',
-		],
-	},
-	{
-		title: 'pokedex',
-		deployed: true,
-		sourceCode: 'https://github.com/devtsp/pokedex-vanilla-client',
-		site: 'https://devtsp.github.io/pokedex-vanilla-client/',
-		previews: [
-			'/img/project%20previews/pokedex/1.jpg',
-			'/img/project%20previews/pokedex/2.jpg',
 		],
 	},
 ];
