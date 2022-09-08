78547d60b099f675e032945a25ac8c2bc849fe69
Tomas <paeztms@gmail.com>
2022-09-08 14:06:51 -0300

    update site deploys to vercel

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,79 +1,79 @@
 const projects = [
-	{
-		title: 'somos más -  ong',
-		deployed: true,
-		sourceCode: 'https://github.com/devtsp/ong-somos-mas-client',
-		site: 'https://ong-somos-mas-client.vercel.app/',
-		previews: [
-			'/img/project%20previews/somos_mas/1.jpg',
-			'/img/project%20previews/somos_mas/2.jpg',
-			'/img/project%20previews/somos_mas/3.jpg',
-			'/img/project%20previews/somos_mas/4.jpg',
-			'/img/project%20previews/somos_mas/5.jpg',
-		],
-	},
-	{
-		title: 'personal transaction manager',
-		deployed: true,
-		sourceCode: 'https://github.com/devtsp/stocktaking-server',
-		site: 'https://devtsp.github.io/stocktaking-client/',
-		previews: [
-			'/img/project%20previews/stocktaking/1.jpg',
-			'/img/project%20previews/stocktaking/2.jpg',
-			'/img/project%20previews/stocktaking/3.jpg',
-			'/img/project%20previews/stocktaking/4.jpg',
-		],
-	},
-	{
-		title: 'post blog',
-		deployed: true,
-		sourceCode: 'https://github.com/devtsp/blog-client',
-		site: 'https://devtsp.github.io/blog-client/',
-		previews: [
-			'/img/project%20previews/blog/1.jpg',
-			'/img/project%20previews/blog/2.jpg',
-			'/img/project%20previews/blog/3.jpg',
-		],
-	},
-	{
-		title: 'currencies exchange rates/conversor',
-		deployed: true,
-		sourceCode: 'https://github.com/devtsp/exchange-rates-client',
-		site: 'https://devtsp.github.io/exchange-rates-client/',
-		previews: [
-			'/img/project%20previews/exchange%20rates/1.jpg',
-			'/img/project%20previews/exchange%20rates/2.jpg',
-		],
-	},
-	{
-		title: 'social media posting',
-		deployed: true,
-		sourceCode: 'https://github.com/devtsp/social-media-client',
-		site: 'https://devtsp.github.io/social-media-client',
-		previews: [
-			'/img/project%20previews/social%20media/1.jpg',
-			'/img/project%20previews/social%20media/2.jpg',
-			'/img/project%20previews/social%20media/3.jpg',
-		],
-	},
-	{
-		title: 'pokedex',
-		deployed: true,
-		sourceCode: 'https://github.com/devtsp/pokedex-client',
-		site: 'https://devtsp.github.io/pokedex-client/',
-		previews: ['/img/project%20previews/pokedex/1.jpg'],
-	},
-	{
-		title: 'memotest',
-		deployed: true,
-		sourceCode: 'https://github.com/devtsp/memotest-client',
-		site: 'https://devtsp.github.io/memotest-client/',
-		previews: [
-			'/img/project%20previews/memotest/1.jpg',
-			'/img/project%20previews/memotest/2.jpg',
-			'/img/project%20previews/memotest/3.jpg',
-		],
-	},
+  {
+    title: 'somos más -  ong',
+    deployed: true,
+    sourceCode: 'https://github.com/devtsp/ong-somos-mas-client',
+    site: 'https://ong-somos-mas-client.vercel.app/',
+    previews: [
+      '/img/project%20previews/somos_mas/1.jpg',
+      '/img/project%20previews/somos_mas/2.jpg',
+      '/img/project%20previews/somos_mas/3.jpg',
+      '/img/project%20previews/somos_mas/4.jpg',
+      '/img/project%20previews/somos_mas/5.jpg',
+    ],
+  },
+  {
+    title: 'personal transaction manager',
+    deployed: true,
+    sourceCode: 'https://github.com/devtsp/stocktaking-server',
+    site: 'https://stocktaking-client.vercel.app/login',
+    previews: [
+      '/img/project%20previews/stocktaking/1.jpg',
+      '/img/project%20previews/stocktaking/2.jpg',
+      '/img/project%20previews/stocktaking/3.jpg',
+      '/img/project%20previews/stocktaking/4.jpg',
+    ],
+  },
+  {
+    title: 'post blog',
+    deployed: true,
+    sourceCode: 'https://github.com/devtsp/blog-client',
+    site: 'https://blog-client-ten.vercel.app/',
+    previews: [
+      '/img/project%20previews/blog/1.jpg',
+      '/img/project%20previews/blog/2.jpg',
+      '/img/project%20previews/blog/3.jpg',
+    ],
+  },
+  {
+    title: 'currencies exchange rates/conversor',
+    deployed: true,
+    sourceCode: 'https://github.com/devtsp/exchange-rates-client',
+    site: 'https://devtsp.github.io/exchange-rates-client/',
+    previews: [
+      '/img/project%20previews/exchange%20rates/1.jpg',
+      '/img/project%20previews/exchange%20rates/2.jpg',
+    ],
+  },
+  {
+    title: 'social media posting',
+    deployed: true,
+    sourceCode: 'https://github.com/devtsp/social-media-client',
+    site: 'https://social-media-client-zeta.vercel.app/',
+    previews: [
+      '/img/project%20previews/social%20media/1.jpg',
+      '/img/project%20previews/social%20media/2.jpg',
+      '/img/project%20previews/social%20media/3.jpg',
+    ],
+  },
+  {
+    title: 'pokedex',
+    deployed: true,
+    sourceCode: 'https://github.com/devtsp/pokedex-client',
+    site: 'https://pokedex-client-psi.vercel.app/',
+    previews: ['/img/project%20previews/pokedex/1.jpg'],
+  },
+  {
+    title: 'memotest',
+    deployed: true,
+    sourceCode: 'https://github.com/devtsp/memotest-client',
+    site: 'https://devtsp.github.io/memotest-client/',
+    previews: [
+      '/img/project%20previews/memotest/1.jpg',
+      '/img/project%20previews/memotest/2.jpg',
+      '/img/project%20previews/memotest/3.jpg',
+    ],
+  },
 ];
 
 export default projects;
